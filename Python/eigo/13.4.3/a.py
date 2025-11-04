69f7054cdb0ebb32091a40618a88070f9c6df993
taro <taro333@gmail.com>
2025-11-04 23:35:57 +0900

    fix: いろいろろ

--- Python/eigo/13.4.3/a.py
+++ Python/eigo/13.4.3/a.py
@@ -42,3 +42,39 @@ def score(self):
             return "Two Pairs", 5
         else:
             return "Garbage", 0
+
+
+class PokerApp:
+    def __init__(self):
+        self.dice = Dice()
+        self.money = 100
+        self.interface = PokerInterface()
+
+
+def run(self):
+    while self.money >= 10 and self.interface.wantToPlay():
+        self.playRound()
+        self.interface.close()
+
+
+def playRound(self):
+    self.money = self.money - 10
+    self.interface.setMoney(self.money)
+    self.doRolls()
+    result, score = self.dice.score()
+    self.interface.showResult(result, score)
+    self.money = self.money + score
+    self.interface.setMoney(self.money)
+
+
+def doRolls(self):
+    self.dice.rollAll()
+    roll = 1
+    self.interface.setDice(self.dice.values())
+    toRoll = self.interface.chooseDice()
+    while roll < 3 and toRoll != []:
+        self.dice.roll(toRoll)
+        roll = roll + 1
+        self.interface.setDice(self.dice.values())
+        if roll < 3:
+            toRoll = self.interface.chooseDice()
